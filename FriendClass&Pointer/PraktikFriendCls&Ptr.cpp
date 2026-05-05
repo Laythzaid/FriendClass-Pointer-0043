3c3353d224878e4a03e07ed03e9ac78fabd8a69f
Layth Zaid <Laithhilal23@gmail.com>
2026-05-05 12:20:37 +0700

    Enter Dimensions func

--- FriendClass&Pointer/PraktikFriendCls&Ptr.cpp
+++ FriendClass&Pointer/PraktikFriendCls&Ptr.cpp
@@ -1,4 +1,23 @@
 #include <iostream>
 using namespace std;
 
-class Diamond; // forward declaration
+class rectangle; // forward declaration
+
+class KiteShape {
+private: 
+	float dgl1, dgl2;
+	float sideA, SideB;
+
+public: 
+	void EnterDimns() {
+		cout << "Enter Kite dimensions\n";
+		cout << "Diagonal 1: ";
+		cin >> dgl1;
+		cout << "Diagonal 2: ";
+		cin >> dgl2;
+		cout << "Side A: ";
+		cin >> sideA;
+		cout << "Side B: ";
+		cin >> sideB;
+	}
+};
