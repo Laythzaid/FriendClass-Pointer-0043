137684185b7f3def74c4ba65ba894ab81b2da04f
Layth Zaid <Laithhilal23@gmail.com>
2026-05-05 12:24:45 +0700

    print dimensions func

--- FriendClass&Pointer/PraktikFriendCls&Ptr.cpp
+++ FriendClass&Pointer/PraktikFriendCls&Ptr.cpp
@@ -24,4 +24,16 @@ public:
 	float calcArea() const{
 		return (dgl1 * dgl2);
 	}
+
+	float calcPerimeter() const {
+		return (2 * (sideA + sideB));
+	}
+
+	void printDimns() const {
+		cout << "Kite dimensions\n";
+		cout << "Diagonal 1: " << dgl1 << endl;
+		cout << "Diagonal 2: " << dgl2 << endl;
+		cout << "Side A: " << sideA << endl;
+		cout << "Side B: " << sideB << endl;
+	}
 };
