762a2e6fec95d436babb18b27800870afc8e0635
Layth Zaid <Laithhilal23@gmail.com>
2026-05-05 12:30:46 +0700

    create rectangle class

--- FriendClass&Pointer/PraktikFriendCls&Ptr.cpp
+++ FriendClass&Pointer/PraktikFriendCls&Ptr.cpp
@@ -34,4 +34,36 @@ public:
 		cout << "Area : " << calcArea() << endl;
 		cout << "Perimeter : " << calcPerimeter() << endl;
 	}
+
+	friend float totalPerimeter(rectangle t, KiteShape);
+
 };
+
+class rectangle {
+private:
+	float dgl1, dgl2;
+	float side;
+
+public :
+	void EnterDimns() {
+		cout << "Enter Rectangle dimensions\n";
+		cout << "Diagonal 1: ";
+		cin >> dgl1;
+		cout << "Diagonal 2: ";
+		cin >> dgl2;
+		cout << "Side: ";
+		cin >> side;
+	}
+	float calcArea() const {
+		return (dgl1 * dgl2) / 2;
+	}
+	float calcPerimeter() const {
+		return (4 * side);
+	}
+	void printDimns() const {
+		cout << "Rectangle Shape\n";
+		cout << "Area : " << calcArea() << endl;
+		cout << "Perimeter : " << calcPerimeter() << endl;
+	}
+	friend float totalPerimeter(rectangle t, KiteShape k);
+}
